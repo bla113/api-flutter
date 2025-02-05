3893d956e6e88c36945f195548ef0174be6dea10
bla113 <bla_113@hotmail.com>
2025-02-04 19:36:50 -0600

    Endpoint Register

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,14 +7,37 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    static function users() {
+    static function users()
+    {
 
         $user = User::all();
 
 
-        return response()->json(['user'=>$user]);
+        return response()->json(['user' => $user]);
+    }
 
+    public static function register(request $request)
+    { // REQUEST ES LO QUE SE ENVIA ATRAVEZ DEL ENDPOINT 
 
+        $validate = $request->validate([ //REALIZA LA VALIDACIÓN DE LOS CAMPOS
+            'name' => 'required|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|min:8|string'
+        ]);
 
+        // if ($validate) {
+        //     return response()->json($validate);
+        // }
+
+        $user = User::create(//CREACION DEL USUARIO
+            [
+                'name' =>  $request->name,
+                'email' =>  $request->email,
+                'password' => $request->password,
+            ]
+        );
+        $token = $user->createToken('auth_token')->plainTextToken;// CREA EL TOKEN DE SEGURIDAD DE USO
+
+        return response()->json(['message'=>'Usuario creado correctamente','usuario'=>$user,'token_app'=>$token],200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,4 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::get('/users',[UserController::class,'users'])->name('users');
+Route::post('/register',[UserController::class,'register'])->name('register');
